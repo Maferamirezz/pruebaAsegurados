5658db348c49232bcde9cb5a834ed172ce66a793
María Fernanda Ramírez <40408275+Maferamirezz@users.noreply.github.com>
2022-02-03 08:13:30 -0600

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './components/login/login.component';
+import { AseguradosComponent } from './components/asegurados/asegurados.component';
+import { PaginaPrincipalComponent } from './components/pagina-principal/pagina-principal.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent},
+  { path: 'asegurados/:idAseg', component: AseguradosComponent},
+  { path: 'paginaPrincipal', component: PaginaPrincipalComponent},
+  { path: '**', pathMatch: 'full', redirectTo: 'login' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
